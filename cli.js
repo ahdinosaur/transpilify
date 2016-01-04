42ef7981388e484082f6d7a08fbfba173641741d
mattdesl <dave.des@gmail.com>
2016-01-04 01:31:56 -0500

    testing vinylfs and writing files

--- cli.js
+++ cli.js
@@ -9,8 +9,10 @@ var bl = require('bl')
 var fs = require('fs')
 var through2 = require('through2')
 var mkdirp = require('mkdirp')
+var color = require('term-color')
 
 var args = subarg(process.argv.slice(2), {
+  boolean: [ 'quiet' ],
   alias: {
     transform: 't',
     outDir: [ 'out-dir', 'd' ],
@@ -55,24 +57,33 @@ function run (paths) {
 
   if ((args.outFile && typeof args.outFile !== 'string') ||
       (args.outDir && typeof args.outDir !== 'string')) {
-    return bail('Output option must be a path string')
+    return bail('You must specify a path for --out-file or --out-dir options')
   }
 
   if (args.outFile || (!args.outFile && !args.outDir)) {
     var file = paths[0]
-    var sink
-    if (args.outFile) {
-      sink = path.resolve(basedir, args.outFile, path.basename(file))
-      log(file, sink)
-    } else {
-      sink = process.stdout
-    }
 
-    // support stdout and file writing
-    sink = typeof sink === 'string'
-      ? fs.createWriteStream(sink)
-      : sink
-    transpiler(file).pipe(sink)
+    fs.stat(file, function (err, stat) {
+      if (err && err.code === 'ENOENT') return bail('Missing input file: ' + file)
+      if (err) return bail(err)
+      if (!stat.isFile()) return bail('Input is not a file: ' + file)
+
+      var output = transpiler(file)
+      if (args.outFile) {
+        // Although this is a bit non-standard for unix tools,
+        // we do it because vinyl-fs is using mkdirp under the hood.
+        var newDir = path.resolve(basedir, path.dirname(args.outFile))
+        mkdirp.sync(newDir)
+
+        var sink = path.resolve(basedir, args.outFile)
+        var inPath = path.relative(basedir, file)
+        var outPath = path.relative(basedir, args.outFile)
+        logFile(inPath, outPath)
+        output.pipe(fs.createWriteStream(sink))
+      } else {
+        output.pipe(process.stdout)
+      }
+    })
   } else {
     var outdir = path.resolve(basedir, args.outDir)
     fs.stat(outdir, function (err, stat) {
@@ -83,10 +94,6 @@ function run (paths) {
       toDirectory(paths, args.outDir)
     })
   }
-}
-
-function log (src, dst) {
-  console.error(src, '->', dst)
 }
 
 function toDirectory (paths, dir) {
@@ -117,7 +124,7 @@ function toDirectory (paths, dir) {
 
         var oldPath = path.relative(basedir, file.path)
         var newPath = [ dir, oldPath ].join(path.sep)
-        log(oldPath, newPath)
+        logFile(oldPath, newPath)
         file.contents = buffer
         stream.push(file)
         next()
@@ -133,4 +140,9 @@ function toDirectory (paths, dir) {
 function bail (msg) {
   console.error(typeof msg === 'string' ? ('ERROR: ' + msg) : msg.stack)
   process.exit(1)
+}
+
+function logFile (src, dst) {
+  if (args.quiet) return
+  console.error(src, color.dim('->'), dst)
 }
