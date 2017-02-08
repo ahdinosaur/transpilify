2bd67eaea68bea3b60824195b3606f16d0997033
Augustin Trancart <augustin.trancart@gmail.com>
2017-02-09 00:13:06 +0100

    Add test for cli usage with directory input (#5)

--- test/index.js
+++ test/index.js
@@ -3,10 +3,13 @@ var test = require('tape')
 var path = require('path')
 var fs = require('fs')
 var bl = require('bl')
+var childProcess = require('child_process')
 var uppercaser = require('./uppercase')
 var babelify = require('babelify').configure({
   presets: 'es2015'
 })
+var vinylfs = require('vinyl-fs')
+var through2 = require('through2')
 
 test('handles simple transform function', function (t) {
   t.plan(1)
@@ -55,4 +58,37 @@ test('simple transpiler using browserify transforms', function (t) {
     if (err) return t.fail(err)
     t.equal(body.toString(), expected, 'matches source output')
   }))
+})
+
+test('cli usage: check directory call', function (t) {
+  var tmpDirName = 'tmp'
+  childProcess.execFile(
+    './cli.js',
+    ['test/fixtures/child', '-d', tmpDirName, '-t', './test/uppercase.js'],
+    {},
+    function (error, stdout, stderr) {
+      if (error) {
+        t.fail(error)
+        return
+      }
+
+      // assert stuff
+      vinylfs.src([tmpDirName + '/**/*'])
+      .pipe(through2.obj(function (file, enc, next) {
+        var actualFile = fs.readFileSync(file.path)
+        var expectedFileName = './test/fixtures/child-expected/' + file.stem + file.extname
+        var expectedFile = fs.readFileSync(expectedFileName)
+        t.equal(actualFile.toString(), expectedFile.toString(), file.stem + file.extname)
+        next(null)
+      }, function () {
+        // clean test
+        childProcess.exec('rm -r ' + tmpDirName, function (error, stdout, stderr) {
+          if (error) {
+            t.fail(error)
+          }
+          t.end()
+        })
+      }))
+    }
+  )
 })
